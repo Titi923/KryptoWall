97f5b3de6e1038eaf1a51190f1272e415124ebfd
titi923 <petrisor.buciuta92@gmail.com>
2023-04-02 22:36:10 +0300

    Setup

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+// This is for allowing the app to export all of the components from a single line
+export { default as Loader } from './Loader';
+export { default as Navbar } from './Navbar';
+export { default as Footer } from './Footer';
+export { default as Services } from './Services';
+export { default as Transactions } from './Transactions';
+export { default as Welcome } from './Welcome';
